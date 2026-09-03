e42dee13de42f5d957283d662b48e0bf8c6594dd
Elliott Rolling <elliottjrolling4@gmail.com>
2026-09-03 09:43:41 -0500

    Lab Assignment 01

--- Lab01.py
+++ Lab01.py
@@ -1,1 +1,13 @@
-print("hello world!")
+'''
+Author : Elliott Rolling
+KUID: e857r393
+Date: 9/1/2026
+Lab: Lab01
+Last modified: 9/1/2026
+Purpose: Displaying Name, Major, and Hobbies
+'''
+name = "Elliott Rolling"
+major = "Computer Science Major"
+hobbies = "\n\tVideo Games \n\tMagic The Gathering \n\tMotorsports"
+print("Hello my name is", name, "\nI am a", major, "\nMy hobbies are:",hobbies)
+print("Goodbye")
